4c60f75f506c11230eb4798f53fccca2c486f605
DigitalPotential <Carlos@digitalpotential.se>
2023-06-20 23:14:30 +0200

    init

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function auth () {
+    return (
+        <div className="relative h-full w-full bg-[url(@images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        
+        </div>
+    );
+    }
