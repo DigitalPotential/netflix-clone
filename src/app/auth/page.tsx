10290e252c32e0a548fd054b9b8d4e0cf211f096
DigitalPotential <Carlos@digitalpotential.se>
2023-06-21 21:39:19 +0200

    login component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,7 +4,24 @@ export default function auth() {
             className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover"
             style={{ backgroundImage: `url('/images/hero.jpg')` }}
         >
-            hello
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img
+                        src="/images/logo.png"
+                        alt="site-logo"
+                        className="h-12"
+                    />
+                </nav>
+                <div>
+                    <div className="flex justify-center">
+                        <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                            <h2 className="text-white text-4xl mb-8 font-semibold">
+                                Sign In
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
