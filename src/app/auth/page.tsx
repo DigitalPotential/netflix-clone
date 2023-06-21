a7e4eace456d1bdf0743b9228fea29ef1a80a07c
DigitalPotential <Carlos@digitalpotential.se>
2023-06-21 21:30:15 +0200

    imported image correctly

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,10 @@
-export default function auth () {
+export default function auth() {
     return (
-        <div className="relative h-full w-full bg-[url(@images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-        
+        <div
+            className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover"
+            style={{ backgroundImage: `url('/images/hero.jpg')` }}
+        >
+            hello
         </div>
     );
-    }
+}
