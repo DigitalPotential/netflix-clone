b9599dca09b361ba2a836918f2eef03110f63790
DigitalPotential <Carlos@digitalpotential.se>
2023-06-24 15:08:06 +0200

    feat: create account button

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -48,12 +48,18 @@ export default function Auth() {
                                         setPassword(ev.target.value)
                                     }
                                     type="password"
-                                    value="{email}"
+                                    value="{password}"
                                 />
                             </div>
                             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                                 Login
                             </button>
+                            <p className="text-neutral-500 mt-12">
+                                First time using Netflix?
+                                <span className="text-white ml-1 hover:underline cursor-pointer">
+                                    Create an account
+                                </span>
+                            </p>
                         </div>
                     </div>
                 </div>
