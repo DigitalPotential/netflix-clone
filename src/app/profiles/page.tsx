b693082735d924d331da0c0706729f3f2c93cd7a
DigitalPotential <Carlos@digitalpotential.se>
2023-07-02 21:16:23 +0200

    fix: uppercase "Profiles"

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { redirect, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-export default function profiles() {
+export default function Profiles() {
     const { data: session } = useSession({
         required: true,
         onUnauthenticated() {
