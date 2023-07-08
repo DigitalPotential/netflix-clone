434afb21c0b518267c6900b6c17db517b5a11f6d
DigitalPotential <Carlos@digitalpotential.se>
2023-07-08 22:02:08 +0200

    feat: movie api

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
 import useCurrentUser from "../../../hooks/useCurrentUser"
 import Navbar from '@components/Navbar'
+import BillBoard from '@components/Billboard'
 
 export default function Home()  {
 
@@ -18,6 +19,7 @@ export default function Home()  {
     return (
         <>
         <Navbar />
+        <BillBoard />
         <div className='h-screen w-screen flex justify-center items-center'>
             <button className='h-10 w-32 bg-white' onClick={() => signOut({ callbackUrl: '/login' })}>Logout!</button>
         </div>
