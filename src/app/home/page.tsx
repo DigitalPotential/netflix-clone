df877e0166bebabc23ac66965ac65fbaf0d23858
DigitalPotential <Carlos@digitalpotential.se>
2023-07-09 22:17:08 +0200

    feat: changed img to Image, added zustand

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation"
 import useCurrentUser from "../../../hooks/useCurrentUser"
 import Navbar from '@components/Navbar'
 import BillBoard from '@components/Billboard'
+import MovieList from '@components/Movielist'
+import useMovieList from "../../../hooks/useMovieList"
 
 export default function Home()  {
 
@@ -15,11 +17,16 @@ export default function Home()  {
         }
     })
     const { data: user } = useCurrentUser();
+    const { data: movies = [] } = useMovieList();
+
 
     return (
         <>
         <Navbar />
         <BillBoard />
+        <div className="pb-40">
+                <MovieList title="Trending Now" data={movies} />
+        </div>
         <div className='h-screen w-screen flex justify-center items-center'>
             <button className='h-10 w-32 bg-white' onClick={() => signOut({ callbackUrl: '/login' })}>Logout!</button>
         </div>
